0bf25b5114dd5f111ff8ca8dbe7cd1a29c3bbb82
khaledfouda <khaledsfouda@gmail.com>
2024-01-08 14:05:51 -0500

    01/08 - 3

--- cas_etude/new_data/exploring_bed_data.R
+++ cas_etude/new_data/exploring_bed_data.R
@@ -17,10 +17,10 @@ library(feather)
 select <- dplyr::select
 
 # test file
-wgbs_file1 <- './new_data/Gene_data/AML_BM_bp27_meth_methCpG.bed'
-df.test <- import.bed(wgbs_file1) %>% as.data.frame()
+#wgbs_file1 <- './new_data/Gene_data/AML_BM_bp27_meth_methCpG.bed'
+#df.test <- import.bed(wgbs_file1) %>% as.data.frame()
 
-table(df.test$seqnames) %>% as.data.frame() %>% arrange(desc(Freq)) 
+#table(df.test$seqnames) %>% as.data.frame() %>% arrange(desc(Freq)) 
 #wgbs_file2 <- './new_data/Gene_data/TCD8_term_bp174_meth_methCpG.bed'
 
 # take chr1 as a subset
@@ -46,7 +46,7 @@ transform_age <- function(age) {
    return(mean(age_range))
 }
 
-chromosome_to_retain = "chr2"
+chromosome_to_retain = paste0("chr", 4:7)
 gene_data_folder <- 'new_data/Gene_data/'
 skip_check = TRUE
 skip_sex = FALSE
@@ -64,7 +64,7 @@ if(run.the.slow.code == TRUE){
       mutate(across(-c(DONOR_AGE,bedFile, featherFile), as.factor)) %>%
       mutate(DONOR_AGE = sapply(DONOR_AGE, transform_age)) %>%
       filter(! is.na(DONOR_AGE)) ->
-      dat.info
+      dat.info  
    
    skimr::skim(dat.info)
    #--------------------------------------------------------------
@@ -84,25 +84,31 @@ if(run.the.slow.code == TRUE){
              ", # width != 2 is ", sum(gene.dat$width-2),
              ", X/Y = ", dat.info$XY.ratio[i]))
       
-      gene.chr <- gene.dat %>% filter(seqnames == chromosome_to_retain) %>% select(start, score)
-      write_feather(gene.chr, paste0(dat.info$featherFile[i],"_",chromosome_to_retain,"_.feather"))
+      for(chromosome in chromosome_to_retain){
+         gene.dat %>% filter(seqnames == chromosome) %>% select(start, score) %>% 
+            write_feather(path=paste0(dat.info$featherFile[i],"_",chromosome,"_.feather"))
+      }
       print("...")
    }
    saveRDS(dat.info, "new_data/sample_info.rds")
 }
 #-----------------------------------------------------------------------------
 dat.info = readRDS("new_data/sample_info.rds")
-run.the.second.slow.code = FALSE
+run.the.second.slow.code = TRUE
+skip_other_parts = TRUE
+chromosome = "chr3"
+cut_off = 1e6
 #------------------------------------------------------------------------------
 if(run.the.second.slow.code == TRUE){
    X.dat <- 
       dat.info %>% 
-      transmute(MALE = ifelse(is.na(DONOR_SEX), XY.ratio < 20, DONOR_SEX == "Male" ) + 0,
+      transmute(SAMPLE_ID = 1:nrow(dat.info),
+                MALE = ifelse(is.na(DONOR_SEX), XY.ratio < 20, DONOR_SEX == "Male" ) + 0,
                 AGE = DONOR_AGE,
                 AML = (DISEASE %in% c("Acute myeloid leukemia", "Acute Myeloid Leukemia")) + 0,
                 APL = (DISEASE == "Acute promyelocytic leukemia" ) + 0,
                 BONE_MARROW = (TISSUE_TYPE == "Bone marrow") + 0,
-                CH1_FILE = featherFile) 
+                CH1_FILE = paste0(featherFile,"",chromosome,".feather") ) 
    #-------------------------------------------------------------------------------
    # I need a list of common positions in Chromosome 1 for all patients.
    position_list = list()
@@ -115,7 +121,7 @@ if(run.the.second.slow.code == TRUE){
    position_list_2M = list()
    idx = 1
    for(i in 1:nrow(X.dat)){
-      if(X.dat$length[i] > 2e6){
+      if(X.dat$length[i] > cut_off){
          position_list_2M[[idx]] = position_list[[i]]
          idx = idx + 1
       }
@@ -131,7 +137,7 @@ if(run.the.second.slow.code == TRUE){
    N = length(common_positions)
    m = length(position_list_2M)
    print(N)
-   X.dat_common = X.dat %>% filter(length > 2e6) %>% mutate(length = (length - mean(length))/sd(length) )
+   X.dat_common = X.dat %>% mutate(length = (length - mean(length))/sd(length) )
    # My Y data will be a matrix 
    Y.dat_common <- matrix(NA, nrow = m, ncol = N)
    for(i in 1:nrow(X.dat_common)){
@@ -141,58 +147,61 @@ if(run.the.second.slow.code == TRUE){
          Y.dat_common[i,]
    }
    Y.dat_common = Y.dat_common / 1000
-   saveRDS(sort(common_positions), "new_data/sites_common.rds")
-   saveRDS(as.matrix(select(X.dat_common,-CH1_FILE)), "new_data/Xdat_common.rds")
-   saveRDS(Y.dat_common, "new_data/Ydat_common.rds")
+   saveRDS(sort(common_positions), paste0("new_data/sites_common_chr1",chromosome,".rds"))
+   saveRDS(as.matrix(select(X.dat_common,-CH1_FILE)), paste0("new_data/Xdat_common_chr1",chromosome,".rds"))
+   saveRDS(Y.dat_common, paste0("new_data/Ydat_common_chr1",chromosome,".rds"))
    #---------------------------------------------------------------------------------
-   # Y2.1 :  Merge chromosomes together with different positions but ordered! Take the first non-NA sub-matrix
-   # Include the two patients with short chromosome 1 (ie, length is less than 2M)
-   lengths = unlist(lapply(position_list, length)) %>% sort
-   N = min(lengths)
-   m = length(position_list)
-   X.dat_ordered1 = X.dat %>% mutate(length = (length - mean(length))/sd(length) )
-   Y.dat_ordered1 = matrix(NA, nrow = m, ncol = N)
-   for(i in 1:nrow(X.dat_ordered1)){
-      (read_feather(X.dat_ordered1$CH1_FILE[i]) %>%
-          arrange(start))$score[1:N] ->
-         Y.dat_ordered1[i,]
+   if(skip_other_parts == FALSE){
+      
+      # Y2.1 :  Merge chromosomes together with different positions but ordered! Take the first non-NA sub-matrix
+      # Include the two patients with short chromosome 1 (ie, length is less than 2M)
+      lengths = unlist(lapply(position_list, length)) %>% sort
+      N = min(lengths)
+      m = length(position_list)
+      X.dat_ordered1 = X.dat %>% mutate(length = (length - mean(length))/sd(length) )
+      Y.dat_ordered1 = matrix(NA, nrow = m, ncol = N)
+      for(i in 1:nrow(X.dat_ordered1)){
+         (read_feather(X.dat_ordered1$CH1_FILE[i]) %>%
+             arrange(start))$score[1:N] ->
+            Y.dat_ordered1[i,]
+      }
+      Y.dat_ordered1 = Y.dat_ordered1 / 1000
+      saveRDS(as.matrix(select(X.dat_ordered1,-CH1_FILE)), "new_data/Xdat_ordered1.rds")
+      saveRDS(Y.dat_ordered1, "new_data/Ydat_ordered1.rds")
+      #------------------------------------------------------------------------------------------------------
+      # Y2.2 :  Merge chromosomes together with different positions but ordered! Take the first non-NA sub-matrix
+      # consider chromosomes with more than 2M genes (ie, exclude two subjects)
+      lengths = unlist(lapply(position_list_2M, length)) %>% sort
+      N = min(lengths)
+      m = length(position_list_2M)
+      X.dat_ordered2 = X.dat_common %>% mutate(length = (length - mean(length))/sd(length) )
+      Y.dat_ordered2 = matrix(NA, nrow = m, ncol = N)
+      for(i in 1:nrow(X.dat_ordered2)){
+         (read_feather(X.dat_ordered2$CH1_FILE[i]) %>%
+             arrange(start))$score[1:N] ->
+            Y.dat_ordered2[i,]
+      }
+      Y.dat_ordered2 = Y.dat_ordered2 / 1000
+      saveRDS(as.matrix(select(X.dat_ordered2,-CH1_FILE)), "new_data/Xdat_ordered2.rds")
+      saveRDS(Y.dat_ordered2, "new_data/Ydat_ordered2.rds")
    }
-   Y.dat_ordered1 = Y.dat_ordered1 / 1000
-   saveRDS(as.matrix(select(X.dat_ordered1,-CH1_FILE)), "new_data/Xdat_ordered1.rds")
-   saveRDS(Y.dat_ordered1, "new_data/Ydat_ordered1.rds")
-   #------------------------------------------------------------------------------------------------------
-   # Y2.2 :  Merge chromosomes together with different positions but ordered! Take the first non-NA sub-matrix
-   # consider chromosomes with more than 2M genes (ie, exclude two subjects)
-   lengths = unlist(lapply(position_list_2M, length)) %>% sort
-   N = min(lengths)
-   m = length(position_list_2M)
-   X.dat_ordered2 = X.dat_common %>% mutate(length = (length - mean(length))/sd(length) )
-   Y.dat_ordered2 = matrix(NA, nrow = m, ncol = N)
-   for(i in 1:nrow(X.dat_ordered2)){
-      (read_feather(X.dat_ordered2$CH1_FILE[i]) %>%
-          arrange(start))$score[1:N] ->
-         Y.dat_ordered2[i,]
-   }
-   Y.dat_ordered2 = Y.dat_ordered2 / 1000
-   saveRDS(as.matrix(select(X.dat_ordered2,-CH1_FILE)), "new_data/Xdat_ordered2.rds")
-   saveRDS(Y.dat_ordered2, "new_data/Ydat_ordered2.rds")
 }
 #------------------------------------------------------------------------------------------------------
 # END
 #######################
-gene.chr1$start %>% range()
-
-#---------------------------------------------------------------------------------
-loaded.dat <- read_feather(dat.info$featherFile[i])
-
-dat.info <- readRDS("new_data/sample_info.rds")
-
-print(dat.info$DONOR_SEX[i])
-assign_sex(gen.dat.1$seqnames)
-
-
-
-table((gen.dat.1 %>%
-   select(seqnames) %>%
-   filter(seqnames %in% c("chrX", "chrY")))$seqnames)
-
+# gene.chr1$start %>% range()
+# 
+# #---------------------------------------------------------------------------------
+# loaded.dat <- read_feather(dat.info$featherFile[i])
+# 
+# dat.info <- readRDS("new_data/sample_info.rds")
+# 
+# print(dat.info$DONOR_SEX[i])
+# assign_sex(gen.dat.1$seqnames)
+# 
+# 
+# 
+# table((gen.dat.1 %>%
+#    select(seqnames) %>%
+#    filter(seqnames %in% c("chrX", "chrY")))$seqnames)
+# 
--- cas_etude/new_dmr_sites.R
+++ cas_etude/new_dmr_sites.R
@@ -16,18 +16,28 @@ source("methyl_func.R")
 sourceAll(path="../functions/")
 
 
+Y1 = readRDS("new_data/Ydat_common_chr1.rds")
+X1 = readRDS("new_data/Xdat_common_chr1.rds") %>% as.data.frame() #%>% 
+sites1 = readRDS("new_data/sites_common_chr1.rds")
 
-Y = readRDS("new_data/Ydat_common.rds")
-X = readRDS("new_data/Xdat_common.rds") %>%
-   as.data.frame() #%>% 
+Y2 = readRDS("new_data/Ydat_common_chr2.rds")
+X2 = readRDS("new_data/Xdat_common_chr2.rds") %>% as.data.frame() #%>% 
+sites2 = readRDS("new_data/sites_common_chr2.rds")
+all(X1[,1:4]==X2[,1:4])
+Y = cbind(Y1,Y2)
+sites = c(sites1, sites2)
+sites_order = order(sites)
+Y = Y[,sites_order]
+sites = sites[sites_order]
+X=X1
+dim(Y)
    #mutate(AGE = (AGE - mean(AGE))/ sd(AGE) ) %>% 
    #as.matrix()
-sites = readRDS("new_data/sites_common.rds")
 N = ncol(Y)
 K = nrow(Y)
 #--------------------------------
 library(parallel)
-no_cores <- 30                #detectCores() - 1  # Leave one core free for system processes
+no_cores <- 15                #detectCores() - 1  # Leave one core free for system processes
 cl <- makeCluster(no_cores)
 
 Age = X$AGE
@@ -39,16 +49,16 @@ p_values <- parSapply(cl, 1:ncol(Y), function(i) {
 })
 stopCluster(cl)
 adjusted_p_values <- p.adjust(p_values, method = "bonferroni", n = length(p_values))
-saveRDS(p_values, "new_data/p_values.rds")
-saveRDS(adjusted_p_values, "new_data/adjusted_p_values.rds")
+#saveRDS(p_values, "new_data/p_values.rds")
+#saveRDS(adjusted_p_values, "new_data/adjusted_p_values.rds")
 #-------------------------------------------------------------
-significance_threshold = -log10(0.05 / 1381622) 
+significance_threshold = -log10(0.1 / N) 
    #mutate(Category = ifelse(log10(p_values) < significance_threshold, "Significant", "Not Significant"))
 
 data.frame(x = 1:N, Site= sites, NegLogP = - log10(p_values)) ->
    manh.dat
 manh.dat %>% 
-   ggplot(aes(Site, NegLogP)) +
+   ggplot(aes(Site, NegLogP)) +  
    geom_point(alpha = 0.6) +
    #scale_color_manual(values = c("Significant" = "red", "Not Significant" = "blue")) +
    theme_minimal() +
@@ -58,10 +68,10 @@ manh.dat %>%
    #scale_x_continuous(breaks = manh.dat$x, labels = manh.dat$sites)  +
    ggtitle("Manhattan Plot of Methylation Sites") -> p 
    
-ggsave(filename = "manhattan_plot.png", plot = p, width = 10, height = 6, dpi = 300)
-#p
-
-range(p_values,na.rm = T)
+ggsave(filename = "manhattan_plot_chr1_chr2.png", plot = p, width = 10, height = 6, dpi = 300)
+p
+ 
+range(p_values,na.rm = T) 
 
 
 wherenais = which(is.na(p_values))
