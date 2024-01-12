04f0c86d0a719c92e318b0a633cfe9e5bc65734d
khaledfouda <khaledsfouda@gmail.com>
2024-01-12 13:53:26 -0500

    01/12 - 2

--- cas_etude/load_packages_new.R
+++ cas_etude/load_packages_new.R
@@ -17,5 +17,12 @@ library(doParallel)
 library(foreach)
 
 sourceAll(path="../functions/")
+
+source("new_code/clean_v1_1_extract_chromosomes.R")
+source("new_code/clean_v1_2_combine_feathers.R")
+source("new_code/clean_v2_1_extract_chromosomes.R")
+source("new_code/clean_v2_2_combine_feathers.R")
+
 source("new_code/analyze_1_compute_p_values.R/")
-source("new_code")
+source("new_code/analyze_2_compute_dmr.R")
+source("new_code/analyze_3_fit_models.R")
